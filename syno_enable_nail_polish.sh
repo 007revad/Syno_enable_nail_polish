e550609bb7b1b14f7dcab64aff817dab33ef8a54
Dave Russell <39733752+007revad@users.noreply.github.com>
2024-03-02 09:57:55 +1100

    Create syno_enable_nail_polish.sh

---
+++ syno_enable_nail_polish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+Red='\e[41m'
+Green='\e[42m'
+Pink='\e[45m'
+Off='\e[0m'
+
+echo "Doing the magic..." && sleep 1
+
+echo -e "\nYour nail polish is now supported by your Synology NAS :)\n"
+
+echo -e "According to Aerosmith ${Pink}pink${Off} is like ${Red}red${Off} but not quite \n"
+
+echo -e "According to Kermit it ain't easy being ${Green}green${Off} \n"
+
